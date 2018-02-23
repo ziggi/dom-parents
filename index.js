b6d581b90a9b3fe59d5ad82db32c7587f8c509ff
Sergei Marochkin <xziggix@gmail.com>
2018-02-23 21:39:01 +0300

    hotfix

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 export default (element, selector) => {
   const elements = [];
+  let elem = element;
   const isWithSelector = selector !== undefined;
 
-  while ((elem = element.parentElement) !== null) {
+  while ((elem = elem.parentElement) !== null) {
     if (elem.nodeType === Node.ELEMENT_NODE) {
       if (!isWithSelector || elem.matches(selector)) {
         elements.push(elem);
